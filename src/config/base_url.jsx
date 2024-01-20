004659ed77b778f882a994e12b421d70bb56538c
aminahzaharah2 <155809482+aminahzaharah2@users.noreply.github.com>
2024-01-21 03:48:27 +0700

    Update base_url.jsx

--- src/config/base_url.jsx
+++ src/config/base_url.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://frontendabsensi.vercel.app/";
+export const BASE_URL = "https://frontendabsensi.vercel.app";
 export const BACKEND_BASE_URL = "https://presensismkn.000webhostapp.com/";
